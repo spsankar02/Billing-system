7b1e9019762da5811872b0ee67fad075d730a1e2
spsankar02 <159914816+spsankar02@users.noreply.github.com>
2024-02-24 11:37:11 +0530

    Add files via upload

---
+++ src/test/java/com/example/Billing_software/BillingSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Billing_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
