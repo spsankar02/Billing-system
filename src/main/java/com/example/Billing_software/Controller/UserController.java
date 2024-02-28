0e6d665dc2110377d97de2e007eb0a62651da8ae
spsankar02 <159914816+spsankar02@users.noreply.github.com>
2024-02-28 08:42:02 +0530

    Add files via upload

--- src/main/java/com/example/Billing_software/Controller/UserController.java
+++ src/main/java/com/example/Billing_software/Controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.Billing_software.Controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +19,22 @@ public class UserController {
 	private ServiceInterface service;
 	
 	@PostMapping("/addcustomerdetails")
-	public Customer method(@RequestBody Customer value) {
-		return service.create(value);
+	public ResponseEntity<Object> method(@RequestBody Customer value) {
+		try {
+			service.create(value);
+		return  ResponseEntity.ok().body("User details save successfully");}
+		catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating user details");
+		}
+	}
+	
+	@PostMapping("/retrievecustomerdetails")
+	public ResponseEntity<Object> method1(){
+		try {
+		service.findAll();
+		return ResponseEntity.ok().body(service.findAll());}
+		catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving user details");
+		}
 	}
 }
