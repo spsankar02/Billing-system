90c858ca50a49a23f45b0429b5728134e4428f36
spsankar02 <159914816+spsankar02@users.noreply.github.com>
2024-03-20 20:36:02 +0530

    Add files via upload

--- src/main/java/com/example/Billing_software/Repo/Customerrepo.java
+++ src/main/java/com/example/Billing_software/Repo/Customerrepo.java
@@ -1,10 +1,14 @@
 package com.example.Billing_software.Repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Billing_software.Entity.Customer;
 @Repository
 public interface Customerrepo extends JpaRepository<Customer, Long>{
+
+	Customer findBycustomerId(Long customerId);
 
 }
