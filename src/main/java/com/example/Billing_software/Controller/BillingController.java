8c66566a34701c4f6606a5f703d1b9fa5ccd2336
spsankar02 <159914816+spsankar02@users.noreply.github.com>
2024-05-10 18:59:28 +0530

    Add files via upload

---
+++ src/main/java/com/example/Billing_software/Controller/BillingController.java
@@ -0,0 +1,167 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;	
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Service.Serviceclass;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Billing;
+import com.example.demo.model.Product;
+import com.example.demo.repository.BillingRepository;
+import com.example.demo.repository.Productrepo;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
+public class BillingController {
+	
+	@Autowired
+	private BillingRepository billingRepository;
+	
+	@Autowired
+	private Productrepo productrepo;
+	
+	@Autowired
+	private Serviceclass service;
+	
+	//get all billing
+	
+	@GetMapping("/getalluserslist")
+	public List<Billing> getAllBilling(){
+		return billingRepository.findAll();
+	}
+	
+	//create billing rest api
+	
+	@PostMapping("/createuser")
+	public Billing createBilling(@RequestBody Billing billing) {
+		return billingRepository.save(billing);
+	}
+	
+	//get billing by id rest api
+	
+	@GetMapping("/getuserbyid/{id}")
+	public ResponseEntity<Billing> getBillingById(@PathVariable Long id){
+		Billing billing = billingRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Billing not exist with id :" + id));
+		return ResponseEntity.ok(billing);
+	}
+	
+	//update billing rest api
+	
+	@PostMapping("/updateuser/")
+	public ResponseEntity<Billing> updateBilling(@RequestBody Map<String, Object> payload){
+	    Long id = (Long) payload.get("id");
+
+	    Billing billing = billingRepository.findById(id)
+	            .orElseThrow(() -> new ResourceNotFoundException("Billing not exist with id :" + id));
+	    billing.setCustomerName((String) payload.get("customerName"));
+	    billing.setEmailAddress((String) payload.get("emailAddress"));
+	    billing.setAddress((String) payload.get("address"));
+	    billing.setCity((String) payload.get("city"));
+	    billing.setState((String) payload.get("state"));
+	    billing.setPincode(Long.parseLong((String) payload.get("pincode")));
+	    billing.setCountry((String) payload.get("country"));
+	    billing.setRole((String) payload.get("role"));
+	    billing.setCompanyName((String) payload.get("companyName"));
+	    billing.setPhoneNo(Long.parseLong((String) payload.get("phoneNo")));
+	    
+	    Billing updatedBilling = billingRepository.save(billing);
+	    return ResponseEntity.ok(updatedBilling);
+	}
+
+	
+	//delete billing rest api 
+	
+	@PostMapping("/deleteuser/")
+	public ResponseEntity<Map<String, Boolean>> deleteBilling(@RequestBody Map<String, Long> payload){
+	    Long id = payload.get("id"); // Extract the ID from the payload
+	    Billing billing = billingRepository.findById(id)
+	            .orElseThrow(() -> new ResourceNotFoundException("Billing not exist with id :" + id));
+	    billingRepository.delete(billing);
+	    Map<String, Boolean> response = new HashMap<>();
+	    response.put("deleted", Boolean.TRUE);
+	    return ResponseEntity.ok(response);
+	}
+
+
+
+@PostMapping("/addproductdetails")
+public ResponseEntity<Object> method4(@RequestBody Product value) {
+	try {
+		service.create(value);
+	return  ResponseEntity.ok().body("Product details save successfully");}
+	catch(Exception e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating product details");
+	}
+}
+
+@GetMapping("/getproductbyid/{id}")
+public ResponseEntity<Product> getproductById(@PathVariable Long id){
+	Product billing = productrepo.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("Billing not exist with id :" + id));
+	return ResponseEntity.ok(billing);
+}
+
+
+@GetMapping("/retrieveproductdetails")
+public ResponseEntity<Object> method5(){
+	try {
+	service.findAllProduct();
+	return ResponseEntity.ok().body(service.findAllProduct());}
+	catch(Exception e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving product details");
+	}
+}
+
+@PostMapping("/updateproductdetails/{productId}")
+public ResponseEntity<Object> method6(@RequestBody Product value,@PathVariable Long productId){
+	try {
+		service.update(value,productId);
+		return ResponseEntity.ok().body("Product details updated successfully");
+	}catch (IllegalArgumentException e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating Product details: "+e.getMessage());
+	}
+}
+
+@DeleteMapping("/deleteproductdetails/{productId}")
+public ResponseEntity<Object> method7(@PathVariable Long productId){
+	try {
+	service.deleteProductdetails(productId);
+	return ResponseEntity.ok().body("Product details deleted successfully");}
+	catch(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting Product details: "+e.getMessage());
+    }
+}
+
+//@DeleteMapping("/deleteproductdetails/{id}")
+//public ResponseEntity<Map<String, Boolean>> deleteproduct(@PathVariable Long id){
+//	Product billing = productrepo.findById(id)
+//			.orElseThrow(() -> new ResourceNotFoundException("Billing not exist with id :" + id));
+//	productrepo.delete(billing);
+//	Map<String, Boolean> response = new HashMap<>();
+//	response.put("deleted", Boolean.TRUE);
+//	return ResponseEntity.ok(response);
+//}
+
+}
+
+
+
+
+
