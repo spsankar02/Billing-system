5aefd22d2b6ca9a4b95028b35785abb986302bc2
Gomathisankar <159914816+spsankar02@users.noreply.github.com>
2024-05-24 19:08:00 +0530

    Add files via upload

--- src/main/java/com/example/demo/repository/Orderrepo.java
+++ src/main/java/com/example/demo/repository/Orderrepo.java
@@ -3,9 +3,12 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.model.Billing;
 import com.example.demo.model.Order;
 
 @Repository
 public interface Orderrepo extends JpaRepository<Order, Long>{
+
+	Billing findByUser(Object object);
 
 }
